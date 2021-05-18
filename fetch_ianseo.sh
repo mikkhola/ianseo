262e10c96affa7c73c71baaf4a28ce1c11c2f710
Kelly Atkinson <8167044+kellyatkinson@users.noreply.github.com>
2021-05-18 15:24:58 +1200

    Update fetch_ianseo.sh
    Updated to the 14 April 2021 release of IANSEO

--- fetch_ianseo.sh
+++ fetch_ianseo.sh
@@ -4,8 +4,8 @@ cd /
 rm -R -f /app
 mkdir -p /app
 APP_DIR=/app
-wget -q https://www.ianseo.net/Release/Ianseo_20190701.zip && unzip -o Ianseo_20190701.zip -d $APP_DIR
-rm -f Ianseo_20190701.zip
+wget -q https://www.ianseo.net/Release/Ianseo_20210414.zip && unzip -o Ianseo_20210414.zip -d $APP_DIR
+rm -f Ianseo_20210414.zip
 
 rm -f $APP_DIR/Common/config.inc.php 
 
