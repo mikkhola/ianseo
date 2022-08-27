ddb09f8ccfe8b99643fdeb4cf1dd437aa74f481d
TheOnlyTails <theonlytails@theonlytails.com>
2022-08-27 16:56:14 +0300

    updated ianseo to latest

--- fetch_ianseo.sh
+++ fetch_ianseo.sh
@@ -4,8 +4,8 @@ cd /
 rm -R -f /app
 mkdir -p /app
 APP_DIR=/app
-wget -q https://www.ianseo.net/Release/Ianseo_20210414.zip && unzip -o Ianseo_20210414.zip -d $APP_DIR
-rm -f Ianseo_20210414.zip
+wget -q https://www.ianseo.net/Release/Ianseo_20220701.zip && unzip -o Ianseo_20220701.zip -d $APP_DIR
+rm -f Ianseo_20220701.zip
 
 rm -f $APP_DIR/Common/config.inc.php 
 
